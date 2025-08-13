fc2dca623378ec6866a073314c1565b60e6b3f9b
devi5040 <dpraidola@gmail.com>
2025-08-13 10:43:06 +0530

    doc: add JsDoc headers to files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/**
+ * @file app.js
+ * @description Express application configuration and middleware setup.
+ * Initializes routes, middleware, and core application-level settings without starting the server.
+ * This file exports the configured Express app instance for use in server startup or testing environments.
+ *
+ * @version 1.0.0
+ * @created 13-08-2024
+ * @updated 13-08-2024
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
