1169098d422e95b3a35c1f45c429ad97033881b5
devi5040 <dpraidola@gmail.com>
2025-08-14 15:46:45 +0530

    feat: define user model using sequelize

---
+++ src/models/user.js
@@ -0,0 +1,76 @@
+/**
+ * @file user.js
+ * @description Sequelize model definition for the `User` entity.
+ * This model represents users in the system and defines their attributes, data types, and constraints.
+ *
+ * @version v1.0.0
+ * @created 14-08-2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  phone_number: {
+    type: Sequelize.STRING(20),
+    unique: true,
+    allowNull: false,
+    validate: {
+      is: /^\+?[0-9\s]{10,20}$/,
+      notEmpty: true,
+    },
+  },
+  user_name: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  home_address: {
+    type: Sequelize.STRING(255),
+    allowNull: true,
+  },
+  pin_code: {
+    type: Sequelize.STRING(10),
+    allowNull: true,
+    validate: {
+      len: [4, 10],
+    },
+  },
+  language_preference: {
+    type: Sequelize.ENUM("English", "Kannada"),
+    allowNull: false,
+    defaultValue: "English",
+  },
+  profile_image: {
+    type: Sequelize.STRING(255),
+    allowNull: true,
+    validate: {
+      isUrl: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING(255),
+    allowNull: true,
+  },
+  account_status: {
+    type: Sequelize.ENUM("active", "inactive"),
+    allowNull: false,
+    defaultValue: "active",
+  },
+  user_role: {
+    type: Sequelize.ENUM("admin", "user", "shop", "agent"),
+    allowNull: false,
+    defaultValue: "user",
+  },
+});
+
+module.exports = User;
