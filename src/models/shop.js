53fd55caf917e66b2cae8e45572087dc1c4862a5
devi5040 <dpraidola@gmail.com>
2025-08-14 17:05:04 +0530

    feat: define shop model with sequelize

---
+++ src/models/shop.js
@@ -0,0 +1,45 @@
+/**
+ * @file shop.js
+ * @description Represents a shop in the system. Stores details about the shop, its owner and location.
+ *
+ * @version v1.0.0
+ * @created 14-08-2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+
+const Shop = sequelize.define("shop", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  shop_name: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  pin_code: {
+    type: Sequelize.STRING(10),
+    allowNull: true,
+    validate: {
+      len: [4, 10],
+    },
+  },
+  priority: {
+    type: Sequelize.ENUM("1", "2", "3", "4"),
+    defaultValue: "4",
+    allowNull: false,
+  },
+  latitude: {
+    type: Sequelize.DECIMAL(9, 6),
+    allowNull: true,
+  },
+  longitude: {
+    type: Sequelize.DECIMAL(9, 6),
+    allowNull: true,
+  },
+});
+
+module.exports = Shop;
