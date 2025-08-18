849217f92f7af9f216365df3569ce09d1b2c4c28
devi5040 <dpraidola@gmail.com>
2025-08-18 17:11:31 +0530

    feat: add connection db between shop and products model

---
+++ src/models/shopProducts.js
@@ -0,0 +1,46 @@
+/**
+ * @file shopProducts.js
+ * @description This file is a connection between shop and products model. Since same products are available in multiple different shops, the
+ * association between product and shop model is many-to-many.
+ *
+ * @version v1.0.0
+ * @created 18-08-2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+
+const ShopProducts = sequelize.define("shop-products", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: args[0],
+    },
+  },
+  status: {
+    type: Sequelize.ENUM("accepted", "rejected", "pending"),
+    allowNull: false,
+    defaultValue: "pending",
+  },
+  price: {
+    type: Sequelize.DECIMAL(9, 6),
+    allowNull: false,
+    validate: {
+      min: args[0],
+    },
+  },
+  quality: {
+    type: Sequelize.ENUM("premium", "medium", "low"),
+    allowNull: false,
+    defaultValue: "medium",
+  },
+});
+
+module.exports = ShopProducts;
