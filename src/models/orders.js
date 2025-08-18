855ea0880b8730700cdb7ae47cecb4aadf196e48
devi5040 <dpraidola@gmail.com>
2025-08-18 17:42:09 +0530

    feat: add orders model

---
+++ src/models/orders.js
@@ -0,0 +1,37 @@
+/**
+ * @file orders.js
+ * @description This file defines the schema for orders model and exports it.
+ *
+ * @version v1.0.0
+ * @created 18-08-2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+const Sequelize = require("sequelize");
+const sequelize = require("../config/db");
+
+const Orders = sequelize.define("orders", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      min: 0.01,
+    },
+  },
+  collection_centre: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  payment_status: {
+    type: Sequelize.ENUM("paid", "pending"),
+    allowNull: false,
+    defaultValue: "pending",
+  },
+});
+
+module.exports = Orders;
