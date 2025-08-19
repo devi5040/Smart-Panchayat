e1a102a83a31bee8bdb9b1e0de07a732b191c4eb
devi5040 <dpraidola@gmail.com>
2025-08-19 16:32:56 +0530

    feat: add config file for aws s3.

---
+++ src/config/aws/aws.s3.config.js
@@ -0,0 +1,21 @@
+/**
+ * @filename aws.s3.config.js
+ * @description This module configures and exports an AWS S3 client object.  It uses environment variables to securely manage AWS credentials
+ * and the region. This allows for easy interaction with Amazon S3 services within other parts of the application.
+ *
+ * @version v1.0.0
+ * @created August 19, 2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+module.exports = s3;
