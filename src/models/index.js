6cba5abe67865d28b8a208824744054305c7f358
devi5040 <dpraidola@gmail.com>
2025-08-19 10:56:16 +0530

    feat: add association between shops and products model.

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,20 @@
 /**JSDOC needs to be done */
 const Users = require("./user");
 const Shops = require("./shop");
+const Products = require("./products");
+const ShopProducts = require("./shopProducts");
 
-// Associations between shop and user (one-to-one)
 Users.hasOne(Shops, { onDelete: "CASCADE" });
 Shops.belongsTo(Users);
+
+// Many-to-Many: Shops <-> Products.
+// A shop can have multiple products. A product is available in multiple shops.
+// OnDelete: CASCADE and onUpdate: CASCADE -> defaults for many-to-many relationships.s
+Shops.belongsToMany(Products, {
+  through: ShopProducts,
+});
+Products.belongsToMany(Shops, {
+  through: ShopProducts,
+});
 
 module.exports = { Users, Shops };
