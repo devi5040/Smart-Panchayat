8e84d8ef8c8c256f1758aeebe3fc6b6ea562f23d
devi5040 <dpraidola@gmail.com>
2025-08-19 12:38:36 +0530

    doc: add jsdoc to shipmentshopProducts

--- src/models/shipmentShopsProducts.js
+++ src/models/shipmentShopsProducts.js
@@ -1,3 +1,14 @@
+/**
+ * @filename <Your file name>
+ * @description This file defines the Sequelize model for the 'shipment-shop-products' table.  It represents the relationship between shipments
+ * and the products within them, storing the quantity of each product in a shipment.
+ * The model includes validation to ensure the quantity is always a positive decimal value.
+ *
+ * @version v1.0.0
+ * @created Aug 19, 2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+
 const Sequelize = require("sequelize");
 const sequelize = require("../config/db");
 
