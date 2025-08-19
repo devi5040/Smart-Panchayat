eb5e91d5d5cc3284c71e4a7e71e649bd4f33ac4c
devi5040 <dpraidola@gmail.com>
2025-08-19 12:44:25 +0530

    feat: add associations between different models. add Jsdoc comments.

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,14 @@
-/**JSDOC needs to be done */
+/**
+ * @filename index.js
+ * @description This file defines the database relationships between different entities using Sequelize. It establishes associations such as
+ * one-to-one between Users and Shops, many-to-many between Shops and Products, Orders and Products, and Shipments and Shops.
+ * It also defines relationships to manage order items and shipment details, ensuring data integrity and efficient data retrieval.
+ *
+ * @version v1.0.0
+ * @created Aug 19 2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
+
 const Users = require("./user");
 const Shops = require("./shop");
 const Products = require("./products");
@@ -7,6 +17,7 @@ const Orders = require("./orders");
 const OrderItems = require("./orderItems");
 const Shipments = require("./shipments");
 const ShipmentShops = require("./shipmentShops");
+const ShipmentShopProducts = require("./shipmentShopsProducts");
 
 // One-to-One: User <-> Shop
 // A User has exactly one Shop.
@@ -38,4 +49,20 @@ Orders.belongsTo(Users);
 Shipments.belongsToMany(Shops, { through: ShipmentShops });
 Shops.belongsToMany(Shipments, { through: ShipmentShops });
 
-module.exports = { Users, Shops };
+// Many-to-Many: ShipmentShops <-> Products
+// A shipmentShop can have multiple products. A product belongs to multiple shipmentShops.
+// Defaults: onDelete: CASCADE and onUpdate: CASCADE
+ShipmentShops.belongsToMany(Products, { through: ShipmentShopProducts });
+Products.belongsToMany(ShipmentShops, { through: ShipmentShopProducts });
+
+module.exports = {
+  Users,
+  Shops,
+  OrderItems,
+  Orders,
+  Products,
+  Shipments,
+  ShipmentShops,
+  ShipmentShopProducts,
+  ShopProducts,
+};
