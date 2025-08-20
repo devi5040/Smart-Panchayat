dbe628c986c43232bda39ff64977c406a646c0fe
devi5040 <dpraidola@gmail.com>
2025-08-20 11:42:59 +0530

    feat: add route for adding the user.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 
+const authMiddleware = require("../middleware/firebaseAuthMiddleware");
 //import validation middleware and schemas
 const validate = require("../middleware/validation.middleware");
 const fileUploadValidation = require("../utils/validation/fileUploadValidation");
+const userDataValidation = require("../utils/validation/user.validate");
 
 router.post(
   "/signed-url",
   validate(fileUploadValidation),
   userController.getSignedURL
+);
+
+router.post(
+  "/",
+  authMiddleware,
+  validate(userDataValidation),
+  userController.addUser
 );
 
 module.exports = router;
