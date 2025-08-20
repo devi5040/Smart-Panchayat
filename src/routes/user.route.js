14d92679ae2c1c6a53b6623d1644b699f29d9bb4
devi5040 <dpraidola@gmail.com>
2025-08-20 10:23:22 +0530

    fix: add validation middleware to get signed url route.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 
-router.post("/signed-url", userController.getSignedURL);
+//import validation middleware and schemas
+const validate = require("../middleware/validation.middleware");
+const fileUploadValidation = require("../utils/validation/fileUploadValidation");
+
+router.post(
+  "/signed-url",
+  validate(fileUploadValidation),
+  userController.getSignedURL
+);
 
 module.exports = router;
