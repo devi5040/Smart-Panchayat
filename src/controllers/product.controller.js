92f30c06145cf726d2ea0fbb5a0db203fa76fa3c
devi5040 <dpraidola@gmail.com>
2025-08-22 18:25:22 +0530

    feat: add controller layer for getting all products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -12,11 +12,24 @@ exports.getAllProductsByCategory = async (req, res) => {
     logger.error(
       `Internal error while retrieving products by category. ${error}`
     );
+    res.status(500).json({
+      message: "Internal error while retrieving products.",
+      error: error.message,
+    });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await productServices.getAllProducts();
     res
-      .status(500)
-      .json({
-        message: "Internal error while retrieving products.",
-        error: error.message,
-      });
+      .status(200)
+      .json({ message: "Products fetched successfully.", products });
+  } catch (error) {
+    logger.error(`Internal error while fetching all products: ${error}`);
+    res.status(500).json({
+      message: "Internal error while fetching products.",
+      error: error.message,
+    });
   }
 };
