e9944c628648df05f9c4e899e33559febbf1d4dd
devi5040 <dpraidola@gmail.com>
2025-08-22 18:11:54 +0530

    feat: add service layer for getting products by category

---
+++ src/services/product.service.js
@@ -0,0 +1,17 @@
+const { Products } = require("../models");
+
+exports.getProductsByCategory = async (categoryId) => {
+  if (
+    categoryId == 0 ||
+    isNaN(categoryId) ||
+    categoryId == undefined ||
+    categoryId == null
+  )
+    throw new Error("Category id is invalid.");
+  try {
+    const products = await Products.findAll({ where: { categoryId } });
+    return products;
+  } catch (error) {
+    throw error;
+  }
+};
