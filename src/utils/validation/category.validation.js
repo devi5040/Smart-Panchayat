c37c9f4259a79c5288a4f41526350dfcd16bd937
devi5040 <dpraidola@gmail.com>
2025-08-22 15:38:19 +0530

    docs: document the validations for category

--- src/utils/validation/category.validation.js
+++ src/utils/validation/category.validation.js
@@ -1,3 +1,12 @@
+/**
+ * @filename category.validation.js
+ * @description This file defines a Joi schema for validating category data.  It ensures that category objects have a non-empty name and a
+ * valid imageUrl.  The schema provides user-friendly error messages for validation failures.
+ *
+ * @version v1.0.0
+ * @updated August 22, 2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
 const Joi = require("joi");
 
 exports.categoryValidationSchema = Joi.object({
