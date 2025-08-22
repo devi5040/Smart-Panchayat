8359ce04c280de6f3ceccbe2f45e35b60c9fdf04
devi5040 <dpraidola@gmail.com>
2025-08-22 12:19:50 +0530

    feat: add controller layer to get signed url for uploading category image

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -9,11 +9,32 @@ exports.getAllCategories = async (req, res) => {
       .json({ message: "Retrieved all categories successfully.", categories });
   } catch (error) {
     logger.error(`Internal error while getting all categories. ${error}`);
-    res
-      .status(500)
-      .json({
-        message: "Internal error while getting all categories.",
-        error: error.message,
-      });
+    res.status(500).json({
+      message: "Internal error while getting all categories.",
+      error: error.message,
+    });
+  }
+};
+
+exports.getSignedUrl = async (req, res) => {
+  const { fileName, fileType } = req.body;
+  try {
+    const { signedURL, fileUrl } = await categoryServices.getSignedUrl(
+      fileName,
+      fileType
+    );
+    res.status(200).json({
+      message: "Signed URL received successfully.",
+      signedURL,
+      fileUrl,
+    });
+  } catch (error) {
+    logger.error(
+      `Internal error while receiving signed url from amazon s3: ${error}`
+    );
+    res.status(500).json({
+      message: "Internal error while receiving signed URL from amazon s3.",
+      error: error.message,
+    });
   }
 };
