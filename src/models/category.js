1b4b2cdd5f525adadb81060e312863002d325a4e
devi5040 <dpraidola@gmail.com>
2025-08-22 15:19:49 +0530

    docs: document the category model

--- src/models/category.js
+++ src/models/category.js
@@ -1,3 +1,13 @@
+/**
+ * @filename category.js
+ * @description This file defines the Sequelize model for the 'category' table in the database.  It sets up the table structure, including
+ * columns for ID, name, and image URL, ensuring data integrity with validation for the URL field.  This model provides an interface for
+ * interacting with category data within the application.
+ *
+ * @version v1.0.0
+ * @updated August 22, 2025
+ * @author Deviprasad Rai P <dpraidola@gmail.com>
+ */
 const Sequelize = require("sequelize");
 
 const sequelize = require("../config/db");
