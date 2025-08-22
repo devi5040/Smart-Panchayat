c01209af66118d3cc9a98706ab178316a84acbdb
devi5040 <dpraidola@gmail.com>
2025-08-22 12:02:17 +0530

    feat: add service layer to get signed url for uploading category image

--- src/services/category.services.js
+++ src/services/category.services.js
@@ -1,9 +1,28 @@
 const { Category } = require("../models/");
+const s3 = require("../config/aws/aws.s3.config");
 
 exports.getCategories = async () => {
   try {
     const categories = await Category.findAll();
     return categories;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getSignedUrl = async (fileName, fileType) => {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `/uploads/category/${Date.now()}-${fileName}`,
+    ContentType: fileType,
+    ACL: "public-read",
+  };
+  try {
+    const signedURL = await s3.getSignedUrlPromise("putObject", params);
+    return {
+      signedURL,
+      fileUrl: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`,
+    };
   } catch (error) {
     throw error;
   }
