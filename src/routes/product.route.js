d3d518af073e0d4a8f3365a60a3664702d21cf97
devi5040 <dpraidola@gmail.com>
2025-08-25 11:53:44 +0530

    feat: add router layer for getting products for a shop

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 
 const authentication = require("../middleware/firebaseAuthMiddleware");
+const authorization = require("../middleware/authorization.middleware");
 
 const productController = require("../controllers/product.controller");
+
+router.get(
+  "/shop",
+  authentication,
+  authorization(["shop"]),
+  productController.getProductForShops
+);
 
 router.get(
   "/:categoryId",
