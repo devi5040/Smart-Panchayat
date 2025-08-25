432c5b8de70c905ec552b0a6a5d68a6ec06a3d5b
devi5040 <dpraidola@gmail.com>
2025-08-25 11:50:27 +0530

    feat: attach shop id to req.user if the user role is shop

--- src/middleware/firebaseAuthMiddleware.js
+++ src/middleware/firebaseAuthMiddleware.js
@@ -10,6 +10,7 @@
 const admin = require("../config/firebase/firebase_config");
 const logger = require("../utils/logger");
 const { getUserByMobileNumber } = require("../services/user.services");
+const { getShopIdbyUserId } = require("../services/shop.service");
 
 const firebaseAuthMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -31,6 +32,10 @@ const firebaseAuthMiddleware = async (req, res, next) => {
     const decodedToken = await admin.auth().verifyIdToken(idToken, true);
     const user = await getUserByMobileNumber(decodedToken.phone_number);
     req.user = { id: user.id, role: user.user_role };
+    if (user.user_role === "shop") {
+      const shop = await getShopIdbyUserId(user.id);
+      req.user.shop = shop.id;
+    }
     next();
   } catch (error) {
     logger.error(`Invalid or expired token: ${error}`);
