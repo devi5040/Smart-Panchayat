188ed1d759f24ccbec68ba5def3065f66b5a3c8f
devi5040 <dpraidola@gmail.com>
2025-08-26 17:18:54 +0530

    feat: add router layer for creating shop

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 const userRoutes = require("./user.route");
 const categoryRoutes = require("./category.route");
 const productRoutes = require("./product.route");
+const shopRoutes = require("./shop.route");
 
 router.use("/user", userRoutes);
 
 router.use("/category", categoryRoutes);
 
 router.use("/product", productRoutes);
+
+router.use("/shop", shopRoutes);
 
 module.exports = router;
---
+++ src/routes/shop.route.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const auth = require("../middleware/firebaseAuthMiddleware");
+const validation = require("../middleware/validation.middleware");
+
+const shopController = require("../controllers/shop.controller");
+
+const shopValidator = require("../utils/validation/shop.validation");
+
+router.post(
+  "/",
+  auth,
+  validation(shopValidator.productDataSchema),
+  shopController.addShop
+);
+
+module.exports = router;
