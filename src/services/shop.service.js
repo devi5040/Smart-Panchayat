0aa00be16e6b7a7ffa4bd6b10c95d2fc2e3251cc
devi5040 <dpraidola@gmail.com>
2025-08-26 17:18:11 +0530

    feat: add services layer for shop creation

--- src/services/shop.service.js
+++ src/services/shop.service.js
@@ -1,4 +1,5 @@
 const { Shops } = require("../models");
+const { ConflictError } = require("../utils/error");
 
 exports.getShopIdbyUserId = async (userId) => {
   if (!userId || isNaN(userId)) throw new Error("User id is invalid");
@@ -6,6 +7,24 @@ exports.getShopIdbyUserId = async (userId) => {
     const shop = await Shops.findOne({ where: { userId } });
     if (!shop || shop?.length == 0) throw new Error("Shop does not found");
     return shop;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.addShop = async (name, pinCode, latitude, longitude, userId) => {
+  try {
+    const shop = await Shops.findAll({ where: { userId } });
+    if (shop || !shop?.length == 0)
+      throw new ConflictError("Shop already exists for the user");
+    const newShop = await Shops.create({
+      shop_name: name,
+      pin_code: pinCode,
+      latitude,
+      longitude,
+      userId,
+    });
+    return newShop;
   } catch (error) {
     throw error;
   }
