8a7a2d460c077ac70bfaecc35e2e29a612521ce3
devi5040 <dpraidola@gmail.com>
2025-08-28 11:57:56 +0530

    feat: add service layer for getting user by account status(active/inactive).

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -14,6 +14,7 @@ const { Users } = require("../models");
 const logger = require("../utils/logger");
 const admin = require("firebase-admin");
 const { encryptPassword, comparePasswords } = require("../utils/hashPassword");
+const { NotFoundError, BadRequestError } = require("../utils/error");
 
 exports.getSignedUrlS3 = async (fileName, fileType) => {
   const params = {
@@ -22,16 +23,11 @@ exports.getSignedUrlS3 = async (fileName, fileType) => {
     ContentType: fileType,
     ACL: "public-read",
   };
-
-  try {
-    const signedURL = await s3.getSignedUrlPromise("putObject", params);
-    return {
-      signedURL,
-      fileUrl: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`,
-    };
-  } catch (error) {
-    throw error;
-  }
+  const signedURL = await s3.getSignedUrlPromise("putObject", params);
+  return {
+    signedURL,
+    fileUrl: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`,
+  };
 };
 
 exports.addUser = async ({
@@ -42,31 +38,23 @@ exports.addUser = async ({
   longitude,
   role,
 }) => {
-  try {
-    await Users.create({
-      phone_number: mobileNumber,
-      user_name: name,
-      language_preference: languagePreference,
-      latitude,
-      longitude,
-      role,
-    });
-    logger.info("User created successfully");
-  } catch (error) {
-    throw error;
-  }
+  const data = await Users.create({
+    phone_number: mobileNumber,
+    user_name: name,
+    language_preference: languagePreference,
+    latitude,
+    longitude,
+    role,
+  });
 };
 
 exports.getUserByMobileNumber = async (mobileNumber) => {
   if (!mobileNumber) throw new Error("The mobile number is invalid.");
-  try {
-    const user = await Users.findOne({
-      where: { phone_number: mobileNumber },
-    });
-    return user;
-  } catch (error) {
-    throw error;
-  }
+  const user = await Users.findOne({
+    where: { phone_number: mobileNumber },
+  });
+  if (!user) throw new NotFoundError("User not found");
+  return user;
 };
 
 exports.getUserByID = async (id) => {
@@ -79,43 +67,29 @@ exports.getUserByID = async (id) => {
   if (id === 0) {
     return null;
   }
-  try {
-    const user = await Users.findByPk(id, {
-      attributes: { exclude: ["password"] },
-    });
-    if (!user) return null;
-    return user;
-  } catch (error) {
-    throw error;
-  }
+  const user = await Users.findByPk(id, {
+    attributes: { exclude: ["password"] },
+  });
+  if (!user) throw new Error("User not found");
+  return user;
 };
 
 exports.updateUserDetails = async ({ userId, data }) => {
-  if (userId === null || userId === undefined) {
+  if (userId === null || userId === undefined || userId == 0) {
     throw new Error("Invalid user ID: ID cannot be null or undefined");
   }
-  if (userId === null || userId === undefined) {
-    throw new Error("Invalid user ID: ID cannot be null or undefined");
-  }
-  if (userId === 0) {
-    return null;
-  }
-  try {
-    await Users.update(
-      {
-        home_address: data.home,
-        family_name: data.familyName,
-        pin_code: data.pinCode,
-        profile_image: data.profileImage,
-        password: data.password,
-        latitude: data.latitude,
-        longitude: data.longitude,
-      },
-      { where: { id: userId } }
-    );
-  } catch (error) {
-    throw error;
-  }
+  await Users.update(
+    {
+      home_address: data.home,
+      family_name: data.familyName,
+      pin_code: data.pinCode,
+      profile_image: data.profileImage,
+      password: data.password,
+      latitude: data.latitude,
+      longitude: data.longitude,
+    },
+    { where: { id: userId } }
+  );
 };
 
 exports.setPreferredLanguage = async (userId, prefferedLanguage) => {
@@ -125,22 +99,15 @@ exports.setPreferredLanguage = async (userId, prefferedLanguage) => {
   if (userId === 0) {
     throw new Error("Invalid user ID: must not be 0");
   }
-  try {
-    await Users.update(
-      { language_preference: prefferedLanguage },
-      { where: { id: userId } }
-    );
-    return true;
-  } catch (error) {
-    throw error;
-  }
+  await Users.update(
+    { language_preference: prefferedLanguage },
+    { where: { id: userId } }
+  );
+  return true;
 };
 
 exports.changeUserRole = async (userId, currentRole) => {
   let userRole;
-  if (userId === null || userId === undefined) {
-    throw new Error("Invalid user ID: ID cannot be null or undefined");
-  }
   if (userId === null || userId === undefined) {
     throw new Error("Invalid user ID: ID cannot be null or undefined");
   }
@@ -149,98 +116,76 @@ exports.changeUserRole = async (userId, currentRole) => {
   }
   if (currentRole === "user") userRole = "shop";
   else userRole = "user";
-  try {
-    await Users.update({ user_role: userRole }, { where: { id: userId } });
-  } catch (error) {
-    throw error;
-  }
+  await Users.update({ user_role: userRole }, { where: { id: userId } });
 };
 
 exports.addPassword = async (userId, password) => {
-  if (userId === null || userId === undefined) {
+  if (userId === null || userId === undefined || userId == 0) {
     throw new Error("Invalid user ID: ID cannot be null or undefined");
   }
-  if (userId === null || userId === undefined) {
-    throw new Error("Invalid user ID: ID cannot be null or undefined");
+  const hashedPassword = await encryptPassword(password);
+  const currentPassword = await Users.findByPk(userId, {
+    attributes: ["password"],
+  });
+  if (currentPassword.password !== null) {
+    throw new Error(
+      "User already has a password. Please select update password."
+    );
   }
-  if (userId === 0) {
-    return null;
-  }
-  try {
-    const hashedPassword = await encryptPassword(password);
-    const currentPassword = await Users.findByPk(userId, {
-      attributes: ["password"],
-    });
-    if (currentPassword.password !== null) {
-      throw new Error(
-        "User already has a password. Please select update password."
-      );
-    }
-    await Users.update({ password: hashedPassword }, { where: { id: userId } });
-  } catch (error) {
-    throw error;
-  }
+  await Users.update({ password: hashedPassword }, { where: { id: userId } });
 };
 
 exports.updatePassword = async (userId, oldPassword, newPassword) => {
-  if (userId === null || userId === undefined) {
+  if (userId === null || userId === undefined || userId == 0) {
     throw new Error("Invalid user ID: ID cannot be null or undefined");
   }
-  if (userId === null || userId === undefined) {
-    throw new Error("Invalid user ID: ID cannot be null or undefined");
-  }
-  if (userId === 0) {
-    return null;
-  }
-  try {
-    const currentUser = await Users.findByPk(userId, {
-      attributes: ["password"],
-    });
-    const passwordMatch = await comparePasswords(
-      oldPassword,
-      currentUser.password
+  const currentUser = await Users.findByPk(userId, {
+    attributes: ["password"],
+  });
+  const passwordMatch = await comparePasswords(
+    oldPassword,
+    currentUser.password
+  );
+  if (!passwordMatch) {
+    throw new Error(
+      "The old password you entered doesn't match with the saved password."
     );
-    if (!passwordMatch) {
-      throw new Error(
-        "The old password you entered doesn't match with the saved password."
-      );
-    }
-    const newHashedPassword = await encryptPassword(newPassword);
-    const isSamePassword = await comparePasswords(
-      newPassword,
-      currentUser.password
-    );
-    if (isSamePassword) {
-      throw new Error(
-        "You cannot enter same password as previous password. Please change new password."
-      );
-    }
-    await Users.update(
-      { password: newHashedPassword },
-      { where: { id: userId } }
-    );
-  } catch (error) {
-    throw error;
   }
+  const newHashedPassword = await encryptPassword(newPassword);
+  const isSamePassword = await comparePasswords(
+    newPassword,
+    currentUser.password
+  );
+  if (isSamePassword) {
+    throw new Error(
+      "You cannot enter same password as previous password. Please change new password."
+    );
+  }
+  await Users.update(
+    { password: newHashedPassword },
+    { where: { id: userId } }
+  );
 };
 
 exports.getAllUsers = async () => {
-  try {
-    const users = await Users.findAll();
-    return users;
-  } catch (error) {
-    throw error;
-  }
+  const users = await Users.findAll();
+  if (users) throw new NotFoundError("users not found");
+  return users;
 };
 
 exports.logoutUser = async (idToken) => {
-  try {
-    if (!idToken) throw new Error("No token provided.");
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    const uid = decodedToken.uid;
-    await admin.auth().revokeRefreshTokens(uid);
-    return;
-  } catch (error) {
-    throw error;
-  }
+  if (!idToken) throw new Error("No token provided.");
+  const decodedToken = await admin.auth().verifyIdToken(idToken);
+  const uid = decodedToken.uid;
+  await admin.auth().revokeRefreshTokens(uid);
+  return true;
+};
+
+exports.getUsersByStatus = async (status) => {
+  if (!status) throw new Error("status is not provided");
+  if (status !== "active" && status !== "inactive")
+    throw new BadRequestError("Status should either be active or inactive.");
+  const users = await Users.findAll({ where: { account_status: status } });
+  if (!users) throw new Error("Users data is invalid");
+  return users;
 };
