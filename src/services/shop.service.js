8e0d22c4cc98bb147b0003c90323dd5e1933a795
devi5040 <dpraidola@gmail.com>
2025-08-28 13:42:17 +0530

    feat: add service layer for adding remarks

--- src/services/shop.service.js
+++ src/services/shop.service.js
@@ -1,46 +1,40 @@
-const { Shops } = require("../models");
-const { ConflictError, NotFoundError } = require("../utils/error");
+const { Shops, Users, ShipmentShops } = require("../models");
+const {
+  ConflictError,
+  NotFoundError,
+  BadRequestError,
+} = require("../utils/error");
 
 exports.getShopIdbyUserId = async (userId) => {
   if (!userId || isNaN(userId)) throw new Error("User id is invalid");
-  try {
-    const shop = await Shops.findOne({ where: { userId } });
-    if (!shop || shop?.length == 0) throw new Error("Shop does not found");
-    return shop;
-  } catch (error) {
-    throw error;
-  }
+  const user = await Users.findByPk(userId);
+  if (!user) throw new NotFoundError("User not found.");
+  const shop = await Shops.findOne({ where: { userId } });
+  if (!shop) throw new Error("Shop does not found");
+  return shop;
 };
 
 exports.addShop = async (name, pinCode, latitude, longitude, userId) => {
-  try {
-    const shop = await Shops.findAll({ where: { userId } });
-    if (shop || !shop?.length == 0)
-      throw new ConflictError("Shop already exists for the user");
-    const newShop = await Shops.create({
-      shop_name: name,
-      pin_code: pinCode,
-      latitude,
-      longitude,
-      userId,
-    });
-    return newShop;
-  } catch (error) {
-    throw error;
-  }
+  const shop = await Shops.findAll({ where: { userId } });
+  if (shop || !shop?.length == 0)
+    throw new ConflictError("Shop already exists for the user");
+  const newShop = await Shops.create({
+    shop_name: name,
+    pin_code: pinCode,
+    latitude,
+    longitude,
+    userId,
+  });
+  return newShop;
 };
 
 exports.getShops = async () => {
-  try {
-    const shops = await Shops.findAll();
-    return shops;
-  } catch (error) {
-    throw error;
-  }
+  const shops = await Shops.findAll();
+  return shops;
 };
 
 exports.getShopDetails = async (shopId) => {
-  if (!shopId || isNaN(shopId)) throw new Error("Invalid shop id");
+  if (!shopId || isNaN(shopId)) throw new BadRequestError("Invalid shop id");
   const shop = await Shops.findByPk(shopId);
   if (!shop) throw new NotFoundError("Shop not found");
   return shop;
@@ -67,8 +61,19 @@ exports.updateShopDetails = async (
   );
   if (numRowsUpdated == 0) throw new Error("Shop cannot be updated");
   const updatedShop = await Shops.findOne({ where: { id: shopId, userId } });
-  if (!updatedShop) {
-    throw new NotFoundError("Shop not found for this user");
-  }
+  if (!updatedShop) throw new NotFoundError("Shop not found for this user");
   return updatedShop;
+};
+
+exports.addRemarksToShipments = async (shipmentId, remarks) => {
+  if (!shipmentId) throw new Error("shipment id is invalid");
+  const shipment = await ShipmentShops.findByPk(shipmentId);
+  if (!shipment) throw new NotFoundError("Shipment not found");
+  const [numRowsUpdated] = await ShipmentShops.update(
+    { remarks },
+    { where: { id: shipmentId } }
+  );
+  if (numRowsUpdated == 0) throw new Error("No records are updated.");
+  const shipmentData = await ShipmentShops.findByPk(shipmentId);
+  return shipmentData;
 };
