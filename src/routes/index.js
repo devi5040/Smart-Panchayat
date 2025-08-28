d7e6d8765c9cd061dba7a4a15271eaffdead024f
devi5040 <dpraidola@gmail.com>
2025-08-28 18:35:31 +0530

    feat: add router layer for adding an order

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const userRoutes = require("./user.route");
 const categoryRoutes = require("./category.route");
 const productRoutes = require("./product.route");
 const shopRoutes = require("./shop.route");
+const orderRoutes = require("./order.route");
 
 router.use("/user", userRoutes);
 
@@ -11,5 +12,7 @@ router.use("/category", categoryRoutes);
 router.use("/product", productRoutes);
 
 router.use("/shop", shopRoutes);
+
+router.use("/order", orderRoutes);
 
 module.exports = router;
---
+++ src/routes/order.route.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+const validate = require("../middleware/validation.middleware");
+const orderSchema = require("../utils/validation/order.validation");
+
+const orderController = require("../controllers/order.controller");
+
+router.post("/", validate(orderSchema.orderSchema), orderController.addOrder);
+
+module.exports = router;
