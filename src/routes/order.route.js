500d21c09c50f9661a577a0d7812bcb712d63726
devi5040 <dpraidola@gmail.com>
2025-08-29 12:09:23 +0530

    feat: add router layer for getting all orders

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -5,6 +5,8 @@ const orderSchema = require("../utils/validation/order.validation");
 
 const orderController = require("../controllers/order.controller");
 
+router.get("/", orderController.getOrders);
+
 router.post("/", validate(orderSchema.orderSchema), orderController.addOrder);
 
 module.exports = router;
