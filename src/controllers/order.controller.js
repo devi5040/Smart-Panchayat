698cd1bc73a7768d4f2ea559323650eb02ef0a1f
devi5040 <dpraidola@gmail.com>
2025-08-29 12:09:00 +0530

    feat: add controller layer for getting all orders

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -16,3 +16,19 @@ exports.addOrder = async (req, res) => {
     });
   }
 };
+
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await orderServices.getOrders();
+    res
+      .status(200)
+      .json({ message: "✅ Orders data fetched successfully!", orders });
+  } catch (error) {
+    logger.error(`Internal error while fetching orders data: ${error}`);
+    res.status(500).json({
+      message:
+        "⚠️ An internal error occurred while fetching orders data. Please try again later!",
+      error: error.message,
+    });
+  }
+};
