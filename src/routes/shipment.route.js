3c2f745ed4627cd8f4855e21525fedea3121c83e
devi5040 <dpraidola@gmail.com>
2025-09-02 17:38:06 +0530

    feat: add router layer to the add shipment service

--- src/routes/shipment.route.js
+++ src/routes/shipment.route.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
+const validate = require("../middleware/validation.middleware");
+const schema = require("../utils/validation/shipment.validation");
+
 const shipmentController = require("../controllers/shipment.controller");
 
 router.get("/", shipmentController.getShipmentList);
+
+router.post(
+  "/",
+  validate(schema.shipmentSchema),
+  shipmentController.createShipment
+);
 
 module.exports = router;
