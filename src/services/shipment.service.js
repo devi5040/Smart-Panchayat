820686f84726c7f796e36e370e5cae97e5e40bfe
devi5040 <dpraidola@gmail.com>
2025-09-02 17:37:02 +0530

    feat: add service layer to the add shipment service

--- src/services/shipment.service.js
+++ src/services/shipment.service.js
@@ -1,3 +1,4 @@
+const sequelize = require("../config/db");
 const {
   Shipments,
   ShipmentShops,
@@ -5,6 +6,7 @@ const {
   Shops,
   Products,
 } = require("../models");
+const { ConflictError, NotFoundError } = require("../utils/error");
 
 exports.getShipmentList = async () => {
   const shipments = await Shipments.findAll({
@@ -13,4 +15,77 @@ exports.getShipmentList = async () => {
   });
   if (!shipments) throw new Error("Error finding shipments");
   return shipments;
+};
+
+// shipment details with location and transportation mode
+exports.createShipment = async (shipmentDetails, shops) => {
+  const { date, location, collectionCentre, transportationMode } =
+    shipmentDetails;
+  return await sequelize.transaction(async (t) => {
+    const existingShipment = await Shipments.findAll({
+      where: {
+        date,
+        location,
+        collection_centre: collectionCentre,
+        transportation_mode: transportationMode,
+      },
+      transaction: t,
+    });
+    if (existingShipment && existingShipment?.length > 0)
+      throw new ConflictError(
+        "❌ The shipment already exists. Please update the existing shipment."
+      );
+    const shipment = await Shipments.create(
+      {
+        date,
+        location,
+        collection_centre: collectionCentre,
+        transportation_mode: transportationMode,
+      },
+      { transaction: t }
+    );
+    if (!shipment) throw new Error("❌ Shipment not created.");
+    for (const shop of shops) {
+      const shopId = shop.shopId;
+      const shopData = await Shops.findByPk(shopId, { transaction: t });
+      if (!shopData) throw new NotFoundError("❌ Shop does not exist.");
+      const shipmentShops = await ShipmentShops.create(
+        {
+          shopId,
+          shipmentId: shipment.id,
+        },
+        { transaction: t }
+      );
+      if (!shipmentShops) throw new Error("❌ Shipment shops not created.");
+
+      for (const product of shop.products) {
+        const { quantity, productId } = product;
+        const productData = await Products.findByPk(productId, {
+          transaction: t,
+        });
+        if (!productData) throw new NotFoundError("❌ Product does not exist.");
+        const shipmentShopProduct = await ShipmentShopProducts.create(
+          {
+            quantity,
+            productId,
+            shipmentShopId: shipmentShops.id,
+          },
+          { transaction: t }
+        );
+        if (!shipmentShopProduct)
+          throw new Error("❌ Shipment shops not created.");
+      }
+    }
+    const shipmentData = await Shipments.findAll({
+      attributes: ["id", "date", "collection_centre", "transportation_mode"],
+      include: {
+        model: Shops,
+        attributes: ["shop_name", "id", "latitude", "longitude", "pin_code"],
+        through: { attributes: ["status"] },
+      },
+      transaction: t,
+    });
+    if (!shipmentData) throw new Error("Error finding shipments");
+    return shipmentData;
+  });
 };
