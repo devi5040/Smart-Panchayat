08ba5f08a02d5e8f29443ed300ddb271b2e8625b
devi5040 <dpraidola@gmail.com>
2025-09-04 10:20:30 +0530

    fix: add error responses for sending back error messages

--- src/controllers/agent.controller.js
+++ src/controllers/agent.controller.js
@@ -2,18 +2,24 @@ const agentServices = require("../services/agent.services");
 const logger = require("../utils/logger");
 
 exports.createAgent = async (req, res) => {
-  const { mobileNumber, name, latitude, longitude, language } = req.body;
+  const { mobileNumber, name, latitude, longitude, languagePreference } =
+    req.body;
   try {
     const user = await agentServices.addAgent(
       mobileNumber,
       name,
       latitude,
       longitude,
-      language
+      languagePreference
     );
     res.status(201).json({ message: "Agent created successfully!", user });
   } catch (error) {
     const status = error.statusCode || 500;
     logger.error(`Internal error while creating agent: ${error}`);
+    res.status(status).json({
+      message:
+        "⚠️ An internal error occurred while creating the agent. Please try again later.",
+      error: error.message,
+    });
   }
 };
