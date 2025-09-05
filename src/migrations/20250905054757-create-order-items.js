932fc8faa0bbe9d8a0ed0f84dd4e6e1cffd08208
devi5040 <dpraidola@gmail.com>
2025-09-05 11:19:59 +0530

    feat: add the migrations for order-items model

---
+++ src/migrations/20250905054757-create-order-items.js
@@ -0,0 +1,69 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("order-items", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "orders", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "products", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      product_quality: {
+        type: Sequelize.ENUM("premium", "medium", "low"),
+        allowNull: false,
+        defaultValue: "medium",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("order-items");
+  },
+};
