8bdafd73e0db1cb3141dd41f5e6bfcb0742f13f5
devi5040 <dpraidola@gmail.com>
2025-09-05 10:49:55 +0530

    feat: add migrations for users model

---
+++ src/migrations/20250905051503-create-users.js
@@ -0,0 +1,94 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      phone_number: {
+        type: Sequelize.STRING(20),
+        unique: true,
+        allowNull: false,
+      },
+      user_name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      home_address: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      family_name: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      pin_code: {
+        type: Sequelize.STRING(10),
+        allowNull: true,
+      },
+      language_preference: {
+        type: Sequelize.ENUM("English", "Kannada"),
+        allowNull: false,
+        defaultValue: "English",
+      },
+      profile_image: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+        defaultValue: process.env.DEFAULT_PROFILE_IMAGE || null,
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      account_status: {
+        type: Sequelize.ENUM("active", "inactive"),
+        allowNull: false,
+        defaultValue: "active",
+      },
+      user_role: {
+        type: Sequelize.ENUM("admin", "user", "shop", "agent"),
+        allowNull: false,
+        defaultValue: "user",
+      },
+      latitude: {
+        type: Sequelize.DECIMAL(9, 6),
+        allowNull: true,
+      },
+      longitude: {
+        type: Sequelize.DECIMAL(9, 6),
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("users");
+  },
+};
