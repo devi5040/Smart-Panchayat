6e1ce471f7205d23faaec5ecfd9e2b707aad8ff7
devi5040 <dpraidola@gmail.com>
2025-09-05 11:26:16 +0530

    feat: add the migrations for shipment-shops model

---
+++ src/migrations/20250905055341-create-shipment-shops.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("shipment-shops", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      shipmentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "shipments", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      shopId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "shops", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      status: {
+        type: Sequelize.ENUM("pending", "delivered"),
+        allowNull: false,
+        defaultValue: "pending",
+      },
+      remarks: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("shipment-shops");
+  },
+};
