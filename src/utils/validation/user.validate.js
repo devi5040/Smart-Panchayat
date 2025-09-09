8277e12893a9abc96730470b6bcd574eb4511cec
devi5040 <dpraidola@gmail.com>
2025-09-09 22:15:31 +0530

    fix: fix the validation for agent

--- src/utils/validation/user.validate.js
+++ src/utils/validation/user.validate.js
@@ -79,3 +79,24 @@ exports.updatePasswordSchema = Joi.object({
         'Password must be at least 8 characters long and include uppercase, lowercase, and a number',
     }),
 });
+
+exports.createAgentSchema = Joi.object({
+  name: Joi.string().required(),
+  mobileNumber: Joi.string().required(),
+  languagePreference: Joi.string()
+    .required()
+    .messages({ 'string.empty': 'Language Preference is required.' }),
+  latitude: Joi.number().min(-90).max(90).required().messages({
+    'number.base': 'Latitude must be a number',
+    'number.min': 'Latitude cannot be less than -90',
+    'number.max': 'Latitude cannot be greater than 90',
+    'any.required': 'Latitude is required',
+  }),
+
+  longitude: Joi.number().min(-180).max(180).required().messages({
+    'number.base': 'Longitude must be a number',
+    'number.min': 'Longitude cannot be less than -180',
+    'number.max': 'Longitude cannot be greater than 180',
+    'any.required': 'Longitude is required',
+  }),
+});
