d59a51f8b0db9adac1021338211cbfc1c2ab7e4c
devi5040 <dpraidola@gmail.com>
2025-09-16 16:26:29 +0530

    add controller layer for verifying the product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -353,3 +353,17 @@ exports.getVerifiedProducts = async (req, res) => {
       .json({ message: 'Internal error while fetching verified products', error: error.message });
   }
 };
+
+exports.verifyProduct = async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const product = await productServices.verifyProduct(productId);
+    res.status(200).json({ message: 'Verified product successfully', product });
+  } catch (error) {
+    const status = error.statusCode || 500;
+    logger.error(`Internal error while fetching verified products`);
+    res
+      .status(status)
+      .json({ message: 'Internal error while fetching verified products', error: error.message });
+  }
+};
