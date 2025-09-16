3d0b5c92b72520e1e02a083d60d7d0d82de26d64
devi5040 <dpraidola@gmail.com>
2025-09-16 16:26:16 +0530

    add service layer for verifying the product

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -421,3 +421,15 @@ exports.getVerifiedProducts = async () => {
   if (!products) throw new NotFoundError('Products fetch failed');
   return products;
 };
+
+exports.verifyProduct = async (productId) => {
+  const product = await Products.findByPk(productId);
+  if (!product) throw new NotFoundError('Product not found');
+  const [numRowsUpdated] = await Products.update(
+    { isVerified: true },
+    { where: { id: productId } },
+  );
+  if (numRowsUpdated == 0) throw new NoContentError('Product status not updated');
+  const productdata = await Products.findByPk(productId);
+  return productdata;
+};
