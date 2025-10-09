d1c4385e1d3823a550ae16869c8384939f4b888f
devi5040 <dpraidola@gmail.com>
2025-10-09 18:53:41 +0530

    feat: add product model

--- src/models/products.js
+++ src/models/products.js
@@ -9,6 +9,8 @@
  */
 const Sequelize = require('sequelize');
 const sequelize = require('../config/db');
+const esClient = require('../config/elasticsearch.config');
+const Category = require('./category');
 
 const Products = sequelize.define('products', {
   id: {
@@ -37,6 +39,36 @@ const Products = sequelize.define('products', {
     type: Sequelize.BOOLEAN,
     defaultValue: false,
   },
+});
+
+const getCategoryName = async (categoryId) => {
+  const category = await Category.findByPk(categoryId);
+  return category ? category.name : null;
+};
+
+const syncProductsToES = async (product) => {
+  const categoryName = await getCategoryName(product.categoryId);
+
+  await esClient.index({
+    index: 'products',
+    id: product.id,
+    document: {
+      ...product.toJSON(),
+      category: categoryName, // include category name for search
+    },
+  });
+};
+
+Products.afterCreate(async (product) => {
+  await syncProductsToES(product);
+});
+
+Products.afterUpdate(async (product) => {
+  await syncProductsToES(product);
+});
+
+Products.afterDestroy(async (product) => {
+  await esClient.delete({ index: 'products', id: product.id });
 });
 
 module.exports = Products;
