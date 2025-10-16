8b2e1d4e028a5fa61da6dc656ff1e5afb5356e67
devi5040 <dpraidola@gmail.com>
2025-10-16 20:44:14 +0530

    feat: add sync file for elastic search

--- src/utils/elasticsearch.sync.js
+++ src/utils/elasticsearch.sync.js
@@ -6,7 +6,7 @@ async function syncProductsToElasticSearch() {
   const products = await Products.findAll({
     include: {
       model: Category,
-      attributes: ['name'],
+      attributes: ['name_en', 'name_kn'],
     },
   });
 
@@ -14,14 +14,21 @@ async function syncProductsToElasticSearch() {
     const productJSON = product.toJSON();
     const esDoc = {
       ...productJSON,
-      category: productJSON.category ? productJSON.category.name : null,
+      category: {
+        name_en: productJSON.category ? productJSON.category.name_en : null,
+        name_kn: productJSON.category ? productJSON.category.name_kn : null,
+      },
     };
 
     return [{ index: { _index: 'products', _id: product.id } }, esDoc];
   });
 
-  await esClient.bulk({ refresh: true, body });
-  logger.info(`Synced ${products.length} products to Elasticsearch`);
+  if (body.length > 0) {
+    await esClient.bulk({ refresh: true, body });
+    logger.info(`Synced ${products.length} products to Elasticsearch`);
+  } else {
+    logger.error(`Error while syncing data to the elastic search`);
+  }
 }
 
 module.exports = syncProductsToElasticSearch;
