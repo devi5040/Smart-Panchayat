3bd7d8c1f8c85e459706a68d673c37c6834a7f9d
devi5040 <dpraidola@gmail.com>
2025-10-31 19:14:50 +0530

    feat: update controller layer for getting products by status

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -64,12 +64,12 @@ exports.getAllProducts = async (req, res) => {
  */
 exports.getProductForShops = async (req, res) => {
   const shopId = req.user?.shop;
-  const { page } = req.query;
+  const { page, status } = req.query;
   if (!shopId) {
     return res.status(400).json({ message: 'Shop ID is required.' }); //More appropriate status code
   }
   try {
-    const products = await productServices.getProductsForShop(shopId, page);
+    const products = await productServices.getProductsForShop(shopId, page, status);
     res.status(200).json({ message: 'Products fetched successfully.', products });
   } catch (error) {
     logger.error(`Error fetching products for shop ${shopId}: ${error}`);
