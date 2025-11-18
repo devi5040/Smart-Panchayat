8250ba8fd05641278c1e52ac9299e3e9050caa46
devi5040 <dpraidola@gmail.com>
2025-11-18 19:30:34 +0530

    feat: add shopUpdateSchema for validating shop update details

--- src/utils/validation/shop.validation.js
+++ src/utils/validation/shop.validation.js
@@ -23,6 +23,24 @@ exports.shopSchema = Joi.object({
   userId: Joi.number(),
 });
 
+exports.shopUpdateSchema = Joi.object({
+  pinCode: Joi.string().messages({
+    'string.empty': 'Pin code should not be empty',
+  }),
+  latitude: Joi.number().min(-90).max(90).required().messages({
+    'number.base': 'Latitude must be a number',
+    'number.min': 'Latitude cannot be less than -90',
+    'number.max': 'Latitude cannot be greater than 90',
+    'any.required': 'Latitude is required',
+  }),
+  longitude: Joi.number().min(-180).max(180).required().messages({
+    'number.base': 'Longitude must be a number',
+    'number.min': 'Longitude cannot be less than -180',
+    'number.max': 'Longitude cannot be greater than 180',
+    'any.required': 'Longitude is required',
+  }),
+});
+
 exports.remarksSchema = Joi.object({
   remarks: Joi.string().required().messages({
     'string.empty': 'Remarks should not be empty',
