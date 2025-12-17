040ae3df9118138fa1616998503601a66b96e0bf
devi5040 <dpraidola@gmail.com>
2025-12-17 10:37:26 +0530

    feat(search,orders): integrate Elasticsearch user search and syncing, add reusable NoContents UI, enhance order history fetching and localization, update navigation icons, and remove legacy empty-state components

--- src/utils/elasticsearch.sync.js
+++ src/utils/elasticsearch.sync.js
@@ -1,4 +1,4 @@
-const { Products, Category } = require('../models');
+const { Products, Category, Users } = require('../models');
 const esClient = require('../config/elasticsearch.config');
 const logger = require('./logger');
 
@@ -31,4 +31,30 @@ async function syncProductsToElasticSearch() {
   }
 }
 
-module.exports = syncProductsToElasticSearch;
+async function syncUsersToElasticSearch() {
+  const users = await Users.findAll({ where: { user_role: 'user' } });
+
+  const body = users.flatMap((user) => {
+    const esDoc = {
+      id: user.id,
+      user_name_en: user.user_name_en,
+      user_name_kn: user.user_name_kn,
+      family_name_en: user.family_name_en,
+      family_name_kn: user.family_name_kn,
+      home_address_en: user.home_address_en,
+      home_address_kn: user.home_address_kn,
+      phone_number: user.phone_number,
+    };
+
+    return [{ index: { _index: 'users', _id: user.id } }, esDoc];
+  });
+
+  if (body.length > 0) {
+    await esClient.bulk({ refresh: true, body });
+    logger.info(`Synced ${users.length} users to Elastic Search`);
+  } else {
+    logger.error(`Error while syncing data to the elastic search`);
+  }
+}
+
+module.exports = { syncProductsToElasticSearch, syncUsersToElasticSearch };
