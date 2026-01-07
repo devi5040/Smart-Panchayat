f10e93b9bae806654a5b5c1b58f2fe9e4153caa8
devi5040 <dpraidola@gmail.com>
2026-01-07 16:16:01 +0530

    feat: Add update product validation & adjust status validation

--- src/utils/validation/product.validation.js
+++ src/utils/validation/product.validation.js
@@ -13,7 +13,31 @@ exports.productValidation = Joi.object({
   categoryId: Joi.number()
     .required()
     .messages({ 'string.empty': 'Category id should not be empty' }),
-  status: Joi.string().valid('accepted', 'rejected', 'pending').messages({
+  status: Joi.optional().valid('accepted', 'rejected', 'pending').messages({
+    'string.empty': 'Status should not be empty',
+    'string.valid': 'Status should be accepted,rejected or pending.',
+  }),
+});
+
+exports.updateProductValidation = Joi.object({
+  name_en: Joi.string()
+    .required()
+    .messages({ 'string.empty': 'Product name should not be empty.' }),
+  name_kn: Joi.string()
+    .required()
+    .messages({ 'string.empty': 'Product name should not be empty.' }),
+  price: Joi.string().min(0).required().messages({
+    'string.empty': 'Product price should not be empty',
+    'string.min': 'Product price should be greater than 0.',
+  }),
+  // imageUrl: Joi.string().uri().messages({
+  //   'string.empty': 'The image url should not be empty',
+  //   'string.uri': 'Product image should be an url',
+  // }),
+  categoryId: Joi.number()
+    .required()
+    .messages({ 'string.empty': 'Category id should not be empty' }),
+  status: Joi.optional().valid('verified', 'pending').messages({
     'string.empty': 'Status should not be empty',
     'string.valid': 'Status should be accepted,rejected or pending.',
   }),
