298fa0fd5427fd95d39d3e60ed36d55f8d9f1464
devi5040 <dpraidola@gmail.com>
2026-01-21 19:02:52 +0530

    feat: Add routes for collection centre CRUD operations with authentication and authorization

--- src/routes/collection-centre.route.js
+++ src/routes/collection-centre.route.js
@@ -13,4 +13,32 @@ router.get(
   collectionCentreController.getCollectionCentres,
 );
 
+router.get(
+  '/:collectionCentreId',
+  checkAuth,
+  checkPermissions(['admin']),
+  collectionCentreController.getCollectionCentre,
+);
+
+router.post(
+  '/',
+  checkAuth,
+  checkPermissions(['admin']),
+  collectionCentreController.addCollectionCentre,
+);
+
+router.put(
+  '/:collectionCentreId',
+  checkAuth,
+  checkPermissions(['admin']),
+  collectionCentreController.updateCollectionCentre,
+);
+
+router.delete(
+  '/:collectionCentreId',
+  checkAuth,
+  checkPermissions(['admin']),
+  collectionCentreController.deleteCollectionCentre,
+);
+
 module.exports = router;
