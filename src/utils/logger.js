738311904c9d55c07ada607bb326ce8829f0e75f
Deviprasad Rai P <91744010+devi5040@users.noreply.github.com>
2026-01-23 16:03:58 +0530

    Disable logger transports for console and files
    Comment out console and file transports in logger.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -28,9 +28,9 @@ const logger = winston.createLogger({
     logFormat,
   ),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    // new winston.transports.Console(),
+    // new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
